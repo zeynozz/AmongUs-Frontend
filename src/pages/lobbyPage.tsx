0ff6f661c21210cefac453a5b4bb85d093abb0f3
Betül <kulac.esrabetul@hotmail.com>
2024-04-27 22:08:14 +0200

    continued wardrobe feature

--- src/pages/lobbyPage.tsx
+++ src/pages/lobbyPage.tsx
@@ -15,6 +15,7 @@ type Props = {
 export default function LobbyPage({ game, onChangeSetGame }: Props) {
   const [stompClient, setStompClient] = useState(null);
   const [playerColors, setPlayerColors] = useState({});
+  const [isOutfitDialogOpen, setIsOutfitDialogOpen] = useState(false);
   const navigate = useNavigate();
   const { gameCode } = useParams();
 
@@ -97,40 +98,51 @@ export default function LobbyPage({ game, onChangeSetGame }: Props) {
       stompClient.send(`/app/${gameCode}/play`, {}, JSON.stringify(game));
     }
   }
-  function handleChangeOutfit(){
-    navigate("change-outfit");
+  function handleChangeOutfit() {
+    setIsOutfitDialogOpen(true);
+  }
+
+  function handleCloseOutfitDialog() {
+    setIsOutfitDialogOpen(false);
   }
 
   if (!game) {
     return <div>Loading...</div>;
   }
 
-  const isGameReadyToStart = game?.numberOfPlayers === game?.players.length;
+  const isGameReadyToStart = game.numberOfPlayers === game.players.length;
 
   return (
       <div className="space">
         <video autoPlay loop muted className="background-video" src="/public/videos/stars.mp4">
           Your browser does not support the video tag.
         </video>
+        <div className="overlay" style={{ display: isOutfitDialogOpen ? 'flex' : 'none' }}>
+          <div className="outfit-dialog">
+            <div className="close-button" onClick={handleCloseOutfitDialog}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-x-circle" viewBox="0 0 16 16">
+                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+                <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
+              </svg>
+            </div>
+            <p>Hallo</p>
+          </div>
+        </div>
         <div className="lobby-container">
           <div className="spaceship-lobby">
-            <div className="outfit-button-container">
-              <div className="outfit-button" onClick={handleChangeOutfit}>
-                <img src="/public/images/kleiderbügel.png" alt="Change Outfit"/>
-              </div>
+            <div className="spaceship-lobby">
+              {game.players.map((player, index) => (
+                  <div key={player.id} className={`player player-${index + 1}`}>
+                    <img
+                        src={`/public/images/${playerColors[player.id]}Figure.png`}
+                        alt={`${player.username} avatar`}
+                        className="player-avatar"
+                        style={{ transform: `translateX(${player.position.x}px) translateY(${player.position.y}px)` }}
+                    />
+                    <span className="player-name">{player.username}</span>
+                  </div>
+              ))}
             </div>
-            {game.players.map((player, index) => (
-                <div key={player.id} className={`player player-${index + 1}`}>
-                  <img
-                      src={`/public/images/${playerColors[player.id]}Figure.png`}
-                      alt={`${player.username} avatar`}
-                      className="player-avatar"
-                      style={{ transform: `translateX(${player.position.x}px) translateY(${player.position.y}px)` }}
-                  />
-                  <span className="player-name">{player.username}</span>
-                </div>
-            ))}
-
           </div>
           <div className="game-info">
             <div className="game-code">
@@ -147,6 +159,11 @@ export default function LobbyPage({ game, onChangeSetGame }: Props) {
             >
               <img src="/public/images/img.png" alt="Start Game" />
             </button>
+          </div>
+        </div>
+        <div className="outfit-button-container">
+          <div className="outfit-button" onClick={handleChangeOutfit}>
+            <img src="/public/images/kleiderbügel.png" alt="Change Outfit"/>
           </div>
         </div>
       </div>
