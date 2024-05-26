4e5f97beb7136e3b6b1c65d5edec4bceeac1fe42
Betül <kulac.esrabetul@hotmail.com>
2024-05-26 13:58:28 +0200

    layout for chat

--- src/pages/GameMap.tsx
+++ src/pages/GameMap.tsx
@@ -373,20 +373,34 @@ const GameMap: React.FC<Props> = ({ map, playerList, gameCode }) => {
                         <div className="message-container">
                             {messages.map((msg, index) => (
                                 <div key={index} className="message">
-                                    <strong>{msg.sender}:</strong> {msg.content}
+                                    <div className="text-content">
+                                        <strong className="username">{msg.sender}:</strong> {msg.content}
+                                    </div>
                                 </div>
                             ))}
                         </div>
-                        <input
-                            type="text"
-                            value={chatMessage}
-                            onChange={(e) => setChatMessage(e.target.value)}
-                            placeholder="Type your message..."
-                        />
-                        <button onClick={handleSendMessage}>Send</button>
-                        <button className="close-button" onClick={handleCloseChat}>Close</button>
+                        <div className="input-container">
+                            <input
+                                type="text"
+                                value={chatMessage}
+                                onChange={(e) => setChatMessage(e.target.value)}
+                                placeholder="Type message"
+                            />
+                            <button onClick={handleSendMessage}>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-send" viewBox="0 0 16 16">
+                                    <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576zm6.787-8.201L1.591 6.602l4.339 2.76z"/>
+                                </svg>
+                            </button>
+                        </div>
+                        <div className="close-button" onClick={handleCloseChat}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-x-circle" viewBox="0 0 16 16">
+                                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+                                <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
+                            </svg>
+                        </div>
                     </div>
                 </div>
+
             )}
         </div>
     );
